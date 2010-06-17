cb3d82744cf033bf9c5f61d1924287b396eb55d4
Norio Kimura <norioxkimura@gmail.com>
2010-06-17 19:43:47 +0900

    First version that works.

--- abnf/lexer.py
+++ abnf/lexer.py
@@ -21,15 +21,18 @@ tokens = (
 )
 
 
-def Lexer():
+def lexer():
 
     t_IDENT = r"[A-Za-z][A-Za-z0-9-]*"
 
-    t_NEWLINE = r"\n"
+    def t_NEWLINE(t):
+        r"\n+"
+        t.lexer.lineno += len( t.value )
+        return t
 
     t_INTEGER = r"[0-9]+"
 
-    t_STRING = r"\"[^"]*\""
+    t_STRING = r"\"[^\"]*\""
 
     t_VALUE = r"%[bdx][A-Z0-9.-]+"
 
@@ -45,11 +48,17 @@ def Lexer():
 
     t_STAR = r"\*"
 
-    t_LPAREN = r"("
+    t_LPAREN = r"\("
 
-    t_RPAREN = r")"
+    t_RPAREN = r"\)"
 
-    t_LBRACKET = r"["
+    t_LBRACKET = r"\["
 
-    t_RBRACKET = r"]"
+    t_RBRACKET = r"\]"
+
+    t_ignore = " \t\r"
+
+    t_ignore_COMMENT = ";[^\n]*"
+
+    return ply.lex.lex()
 
